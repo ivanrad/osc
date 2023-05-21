bdb1203a53658ced6877919a6b1864ba7ac9f687
ivanrad <ivanra@gmail.com>
2023-05-21 18:00:20 +0100

    remove check for $SSH_TTY

--- growl.sh
+++ growl.sh
@@ -7,9 +7,7 @@ else
 fi
 
 seq=$'\e]9;%s\a'
-if [ -n "$SSH_TTY" ]; then
-  printf "$seq" "$msg" > "$SSH_TTY"
-elif [ -n "$TMUX" ]; then
+if [ -n "$TMUX" ]; then
   client_tty=$(tmux display-message -p '#{client_tty}')
   if [ -n "$client_tty" ]; then
     printf "$seq" "$msg" > "$client_tty"
--- yank.sh
+++ yank.sh
@@ -4,9 +4,7 @@ set -e
 buf=$(cat "${@:--}" | base64 | tr -d '\n')
 # OSC 52 term seq
 seq="\033]52;c;${buf}\a"
-if [ -n "${SSH_TTY}" ]; then
-  printf "$seq" > "${SSH_TTY}"
-elif [ -n "${TMUX}" ]; then
+if [ -n "${TMUX}" ]; then
   client_tty=$(tmux display-message -p '#{client_tty}')
   if [ -n "${client_tty}" ]; then
     printf "$seq" > "${client_tty}"
